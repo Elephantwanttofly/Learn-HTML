05a6629c78b8f13a28d45f7cbbd91f8ee4791d6a
Galang <89914435+Elephantwanttofly@users.noreply.github.com>
2022-01-16 14:46:22 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import "./Assets/App.scss";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="header">header</div>
+
+
+      <div className="Section">
+        <div className="Card Section-body-1">1</div>
+        <div className="Card Section-body-2">2</div>
+        <div className="Card Section-body-3">3</div>
+        <div className="Card Section-body-4">4</div>
+        <div className="Card Section-body-5">5</div>
+        <div className="Card Section-body-6">6</div>
+      </div>
+
+
+<div className="Body">
+  <div className="Body-Anak Body-1">1</div>
+  <div className="Body-Anak Body-2">2</div>
+  <div>1</div>
+  <div>2</div>
+  <div className="Body-Anak Body-3">3</div>
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+</div>
+
+  </div>
+  );
+}
+
+export default App;
